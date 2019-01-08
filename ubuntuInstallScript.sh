cd5c1d2aafada18e30088f9989fe9ceb0e3a633d
Ctrl_Null <raphael@fonseca.tech>
2019-01-08 17:45:06 -0500

    added more, going to get food

--- ubuntuInstallScript.sh
+++ ubuntuInstallScript.sh
@@ -3,27 +3,29 @@
 # Install Script For Ubuntu 18.04
 
 
-# Installing Basic stuff first
+#### Installing Basic stuff first ####
 
-# Papirus Icon Theme
+# Papirus Icon Theme #
 # https://github.com/PapirusDevelopmentTeam/papirus-icon-theme
 sudo add-apt-repository ppa:papirus/papirus \
 sudo apt-get update
 sudo apt-get install -y papirus-icon-theme
 
-# Arc Gtk Theme
+# Arc Gtk Theme #
 # https://github.com/horst3180/arc-theme
 sudo apt install -y arc-theme
 
-# Gnome Tweaks
+# Gnome Tweak tool #
+sudo apt install gnome-tweak-tool
+
+
+##@# Install Dev Stuff ###@
 
 # Git
 sudo apt install -y git
 
 # x-clip
 sudo apt install -y xclip
-
-
 
 # my Key commands
 # ssh-keygen
